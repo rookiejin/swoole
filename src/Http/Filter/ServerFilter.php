e3aa206843c29fedccc5e2bf3b12187e0f6dfcce
winged <ubuntu@ubuntu.winged>
2017-08-17 18:35:53 +0800

    x

---
+++ src/Http/Filter/ServerFilter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/17
+ * Time: 18:00
+ */
+
+namespace Rookiejin\Swoole\Http\Filter;
+
+
+class ServerFilter implements Filter
+{
+    public static function parse($var)
+    {
+        return null;
+    }
+}
--- src/Http/Request.php
+++ src/Http/Request.php
@@ -7,37 +7,53 @@
  */
 
 namespace Rookiejin\Swoole\Http;
+
+use Rookiejin\Swoole\Http\Filter\CookieFilter;
+use Rookiejin\Swoole\Http\Filter\FileFilter;
+use Rookiejin\Swoole\Http\Filter\GetFilter;
 use Rookiejin\Swoole\Http\Filter\HeaderFilter;
+use Rookiejin\Swoole\Http\Filter\MethodFilter;
+use Rookiejin\Swoole\Http\Filter\PostFilter;
+use Rookiejin\Swoole\Http\Filter\RawFilter;
+use Rookiejin\Swoole\Http\Filter\ServerFilter;
 use \Swoole\Http\Request as SwRequest;
 use \Swoole\Http\Response as SwResponse;
 
 class Request
 {
 
-    protected $_get ;
+    protected $_get = [];
 
-    protected $_post ;
+    protected $_post = [];
 
-    protected $_cookies;
+    protected $_cookies = [];
 
-    protected $_session;
+    protected $_files = [];
 
-    protected $_files ;
+    protected $_raw = "";
 
-    protected $_request;
+    protected $_method = [];
 
-    protected $_raw ;
+    protected $_headers = [];
 
-    protected $_method ;
+    protected $_session = [];
 
-    protected $_headers ;
+    protected $_server = null;
 
     const EOF = "\r\n\r\n";
 
-    public function request(SwRequest $request,\Swoole\Http\Response $response) : \Swoole\Http\Response
+    public function request(SwRequest $request, \Swoole\Http\Response $response)
     {
-        $this->_headers = HeaderFilter::parse($request->header);
-        $response->end("hahaha");
-        return $response;
+        var_dump($request);
+        isset($request->get) && $this->_get = GetFilter::parse($request->get);
+        isset($request->_post) && $this->_post = PostFilter::parse($request->post);
+        isset($request->_files) && $this->_files = FileFilter::parse($request->files);
+        isset($request->_cookies) && $this->_cookies = CookieFilter::parse($request->cookie);
+        isset($request->_headers) && $this->_headers = HeaderFilter::parse($request->header);
+        $this->_raw    = RawFilter::parse($request->rawContent());
+        $this->_server = ServerFilter::parse($request->server);
+//        $response->end('1');
+
+//        return new Response();
     }
 }
